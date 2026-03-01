49baa62e9b2cf56d9c669df3243242de31a7484c
has211 <govindhasini571@gmail.com>
2026-03-01 22:46:16 +0530

    Add image erosion processing script

---
+++ 6- Eroded Image.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("C:/Users/LENOVO/Downloads/spam-sentinel-main/node_modules/flatted/python/input.jpg")
+
+if image is None:
+    print("Image not found!")
+    exit()
+
+kernel = np.ones((5,5), np.uint8)
+
+eroded = cv2.erode(image, kernel, iterations=1)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Eroded Image", eroded)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
