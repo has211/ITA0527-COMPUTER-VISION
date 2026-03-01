ae586e30145aba714161d1189108d7a789f12fb1
has211 <govindhasini571@gmail.com>
2026-03-01 22:48:38 +0530

    Add script to dilate an image using OpenCV

---
+++ 8-Dilated Image.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("input.jpg")
+
+if image is None:
+    print("Image not found!")
+    exit()
+
+kernel = np.ones((5,5), np.uint8)
+
+dilated = cv2.dilate(image, kernel, iterations=1)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Dilated Image", dilated)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
