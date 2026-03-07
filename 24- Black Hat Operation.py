92feef368533a3c69586b04df67bb5eeb617a34d
has211 <govindhasini571@gmail.com>
2026-03-07 11:31:25 +0530

    Create 24- Black Hat Operation.py

---
+++ 24- Black Hat Operation.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("input.jpg")
+
+kernel = np.ones((5,5), np.uint8)
+
+blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, kernel)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Black Hat Operation", blackhat)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
