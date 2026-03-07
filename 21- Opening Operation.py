6e62750c3e236692a025522d910db77566749476
has211 <govindhasini571@gmail.com>
2026-03-07 11:29:29 +0530

    Create 21- Opening Operation.py

---
+++ 21- Opening Operation.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("input.jpg")
+
+if image is None:
+    print("Image not found!")
+    exit()
+
+kernel = np.ones((5,5), np.uint8)
+
+opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Opening Operation", opening)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
