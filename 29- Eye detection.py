63293481863720c6c3141c83fc18c0c1b213827b
has211 <govindhasini571@gmail.com>
2026-03-13 19:27:59 +0530

    Create 29- Eye detection.py

---
+++ 29- Eye detection.py
@@ -0,0 +1,15 @@
+import cv2
+
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+
+img = cv2.imread("face.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+eyes = eye_cascade.detectMultiScale(gray)
+
+for (x,y,w,h) in eyes:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("Eye Detection",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
